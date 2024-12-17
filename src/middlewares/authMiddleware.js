23cff9ba04001453f1f783e4d7006b872436805e
John Assefa <johnassefaforbussines@gmail.com>
2024-12-17 02:20:00 -0800

    fixed the authorization format

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -3,7 +3,7 @@ const User = require('../models/user');
 
 const isLogin = async (req, res, next) => {
     try {
-        const token = req.headers['authorization']?.split(' ')[1]; // Token is usually passed as 'Bearer <token>'
+        const token = req.headers['authorization']?.split(' ')[1]; 
         if (!token) {
             return res.status(401).json({ message: 'Unauthorized. Please log in.' });
         }
